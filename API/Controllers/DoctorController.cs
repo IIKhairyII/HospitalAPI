5f79be22ca4d7ef6a8a2ed2738680df739c3daca
mabbas24 <m.abbas@tildetech.ae>
2023-11-13 15:56:26 +0200

    Add doctors api controller

---
+++ API/Controllers/DoctorController.cs
@@ -0,0 +1,35 @@
+﻿using API.Models.Doctor;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/doctor")]
+    [ApiController]
+    public class DoctorController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Doctors()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult Doctors(Doctor doctor)
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public ActionResult Doctors(int key)
+        {
+            return Ok();
+        }
+
+        //[HttpGet]
+        //public ActionResult Doctors([FromQuery] int doctorKey)
+        //{
+        //    return Ok();
+        //}
+    }
+}
