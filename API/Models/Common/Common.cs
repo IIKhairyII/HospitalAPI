59ee3c0c3974509ad111b7eed26675c1207ecb4d
mabbas24 <m.abbas@tildetech.ae>
2023-11-13 15:18:51 +0200

    Add common folder
    Added common folder with common class that includes common info will be used in patient and doctor class

---
+++ API/Models/Common/Common.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models.Common
+{
+    public class Common
+    {
+        [Required]
+        public int Key { get; set; }
+        [Required, MaxLength(30)]
+        public string ID { get; set; }
+        [Required, MaxLength(10)]
+        public string FirstName { get; set; }
+        [Required, MaxLength(10)]
+        public string LastName { get; set; }
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+        [Required, MaxLength(20)]
+        public string PhoneNumber { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+    }
+}
